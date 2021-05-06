295b14ecb8c3b98e7d93db77dd49a9ff9851aa59
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2021-05-06 15:10:08 +0530

    Update telegraph.py

--- SungJinwooRobot/modules/telegraph.py
+++ SungJinwooRobot/modules/telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-darkprince = "SungJinwooRobot"
+darkprince = "MASHA"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=SungJinwooRobot)
 auth_url = r["auth_url"]

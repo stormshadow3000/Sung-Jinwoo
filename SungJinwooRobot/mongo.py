880d892170de00a4101735fd19b5db82a727d54b
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 02:13:45 +0530

    Update and rename mongo.txt to mongo.py

--- SungJinwooRobot/mongo.py
+++ SungJinwooRobot/mongo.py
@@ -21,8 +21,8 @@ from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
-from SaitamaRobot.utils.logger import log
-from SaitamaRobot.conf import get_int_key, get_str_key
+from SungJinwooRobot.utils.logger import log
+from SungJinwooRobot.conf import get_int_key, get_str_key
 
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_PORT = get_int_key("MONGO_PORT")

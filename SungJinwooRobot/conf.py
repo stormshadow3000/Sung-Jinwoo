f0dceeb99d466e6adbb57ac74f8a009c342ec597
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 02:14:15 +0530

    Update and rename conf.txt to conf.py

--- SungJinwooRobot/conf.py
+++ SungJinwooRobot/conf.py
@@ -19,7 +19,7 @@ import sys
 import yaml
 from envparse import env
 
-from SaitamaRobot.utils.logger import log
+from SungJinwooRobot.utils.logger import log
 
 DEFAULTS = {
     'LOAD_MODULES': True,

b098a33475b905ab69c760290a16f2ff2f252631
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2021-07-20 02:25:09 +0530

    Update welcome.py

--- SungJinwooRobot/modules/welcome.py
+++ SungJinwooRobot/modules/welcome.py
@@ -6,9 +6,9 @@ from functools import partial
 from contextlib import suppress
 from io import BytesIO
 from typing import Tuple, Optional
-import SaitamaRobot.modules.sql.welcome_sql as sql
-import SaitamaRobot
-from SaitamaRobot import (
+import SungJinwooRobot.modules.sql.welcome_sql as sql
+import SungJinwooRobot
+from SungJinwooRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -21,18 +21,18 @@ from SaitamaRobot import (
     JOIN_LOGGER,
     telethn,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from SungJinwooRobot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_welcome_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from SungJinwooRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from SungJinwooRobot.modules.helper_funcs.msg_types import get_welcome_type
+from SungJinwooRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
+from SungJinwooRobot.modules.log_channel import loggable
+from SungJinwooRobot.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,

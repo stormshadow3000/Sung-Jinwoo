5ef3e7bcb4aa90cdb0fac631760d652d84d991d5
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 20:56:52 +0530

    Update approve_sql.py

--- SungJinwooRobot/modules/sql/approve_sql.py
+++ SungJinwooRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from SungJinwooRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):

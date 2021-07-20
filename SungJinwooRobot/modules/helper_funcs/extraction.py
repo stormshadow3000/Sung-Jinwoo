6039b69a2d2a40b9b4d18ddd147b75ca6618a712
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 20:52:47 +0530

    Update extraction.py

--- SungJinwooRobot/modules/helper_funcs/extraction.py
+++ SungJinwooRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SaitamaRobot import LOGGER
-from SaitamaRobot.modules.users import get_user_id
+from SungJinwooRobot import LOGGER
+from SungJinwooRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 

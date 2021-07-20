8b26e283c783d3f830614617196db1a4700de83d
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 21:40:43 +0530

    Update userinfo.py

--- SungJinwooRobot/modules/userinfo.py
+++ SungJinwooRobot/modules/userinfo.py
@@ -20,7 +20,7 @@ from SungJinwooRobot.__main__ import STATS, TOKEN, USER_INFO
 import SungJinwooRobot.modules.sql.userinfo_sql as sql
 from SungJinwooRobot.modules.disable import DisableAbleCommandHandler
 from SungJinwooRobot.modules.sql.global_bans_sql import is_user_gbanned
-from SungJinwooRobot.modules.sql.afk_sql import is_afk, check_afk_status
+from SungJinwooRobot.modules.sql.afk_redis import is_afk, check_afk_status
 from SungJinwooRobot.modules.sql.users_sql import get_user_num_chats
 from SungJinwooRobot.modules.helper_funcs.chat_status import sudo_plus
 from SungJinwooRobot.modules.helper_funcs.extraction import extract_user

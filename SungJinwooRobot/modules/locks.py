ae1d31c7823e7ba4267b97b8195fc1fc7cc749d8
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 23:59:35 +0530

    Update locks.py

--- SungJinwooRobot/modules/locks.py
+++ SungJinwooRobot/modules/locks.py
@@ -650,10 +650,10 @@ __mod_name__ = "Locks"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
-    "lock", lock, pass_args=True)  # , filters=Filters.chat_type.groups, run_async=True)
+    "lock", lock, pass_args=True, run_async=True)  # , filters=Filters.chat_type.groups, run_async=True)
 UNLOCK_HANDLER = CommandHandler(
-    "unlock", unlock, pass_args=True)  # , filters=Filters.chat_type.groups, run_async=True)
-LOCKED_HANDLER = CommandHandler("locks", list_locks)  # , filters=Filters.chat_type.groups, run_async=True)
+    "unlock", unlock, pass_args=True, run_async=True)  # , filters=Filters.chat_type.groups, run_async=True)
+LOCKED_HANDLER = CommandHandler("locks", list_lock, run_async=Trues)  # , filters=Filters.chat_type.groups, run_async=True)
 
 dispatcher.add_handler(LOCK_HANDLER)
 dispatcher.add_handler(UNLOCK_HANDLER)

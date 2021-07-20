e52265990b6a4c6731bb313ec5f502c46ed31489
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-20 21:32:52 +0530

    Update chat_status.py

--- SungJinwooRobot/modules/helper_funcs/chat_status.py
+++ SungJinwooRobot/modules/helper_funcs/chat_status.py
@@ -393,6 +393,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SungJinwooRobot .modules import connection
+from SungJinwooRobot.modules import connection
 
 connected = connection.connected

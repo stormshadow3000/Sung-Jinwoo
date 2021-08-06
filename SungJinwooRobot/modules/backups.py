c61ebab67b76b892afe5a23794ecffb80e640024
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2021-08-06 22:04:23 +0530

    Update backups.py

--- SungJinwooRobot/modules/backups.py
+++ SungJinwooRobot/modules/backups.py
@@ -338,15 +338,15 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("SaitamaRobot{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SaitamaRobot-Backup` was specially made for notes.".format(
+        document=open("SungJinwooRobot{}.backup".format(chat_id), "rb"),
+        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SungJinwooRobot-Backup` was specially made for notes.".format(
             chat.title, chat_id, tgl,
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("SaitamaRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("SungJinwooRobot{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
